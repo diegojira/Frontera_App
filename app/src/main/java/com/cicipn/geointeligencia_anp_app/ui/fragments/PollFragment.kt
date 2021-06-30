d60143ed46fb7a77acbbc1349ec17fedcbd9b147
Diego Barrera <barreraangelesdiego@hotmail.com>
2021-06-30 01:24:23 -0500

    Generación de .csv. Falta implementar los datos de la base SQLite
    Diego 30-06

--- app/src/main/java/com/cicipn/geointeligencia_anp_app/ui/fragments/PollFragment.kt
+++ app/src/main/java/com/cicipn/geointeligencia_anp_app/ui/fragments/PollFragment.kt
@@ -1,4 +1,4 @@
-package com.cicipn.geointeligencia_anp_app.ui.fragments
+    package com.cicipn.geointeligencia_anp_app.ui.fragments
 
 import android.content.Context
 import android.content.Intent
@@ -6,17 +6,31 @@ import android.content.SharedPreferences
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.cicipn.geointeligencia_anp_app.R
 import com.cicipn.geointeligencia_anp_app.other.Constants.KEY_NAME
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_poll.*
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.lang.StringBuilder
 import java.net.URI
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,11 +42,9 @@ class PollFragment: Fragment(R.layout.fragment_poll){
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //btnSubmitPoll.setOnClickListener{
-          //  findNavController().navigate(R.id.action_pollFragment_to_routeFragment)
-        //}
 
         //Botón Sección Bienvenido
         btnSeccionBienvenido.setOnClickListener{
@@ -59,15 +71,28 @@ class PollFragment: Fragment(R.layout.fragment_poll){
             val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
 
             if (isConnected){
-                val sendIntent = Intent(Intent.ACTION_SEND)
-                val uri = "file://data/data/com.cicipn.geointeligencia_anp_app/databases/Encuesta"
-                sendIntent.putExtra(Intent.EXTRA_SUBJECT, "GeoInteligencia. Encuesta contestada " +
-                    "por ${sharedPref.getString(KEY_NAME,".")}")
-                sendIntent.putExtra(Intent.EXTRA_TEXT,"Enviado desde Geointeligencia App")
-                sendIntent.putExtra(Intent.EXTRA_STREAM,uri)
-                sendIntent.setType("*/*")
-                startActivity(Intent.createChooser(sendIntent,"Email:"))
+                val data = StringBuilder()
+                data.append("ID,Q1,Q2,Q3")
+                data.append("\nDiego,1,3,5")
+                try {
+                    val out: FileOutputStream = requireContext().openFileOutput("encuesta_geointeligencia.csv", Context.MODE_PRIVATE)
+                    out.write((data.toString()).toByteArray())
+                    out.close()
+                    val fileLocation = File(activity?.filesDir,"encuesta_geointeligencia.csv")
+                    val path: Uri = FileProvider.getUriForFile(requireContext(),"com.example.geointeligencia_ANP_App.FileProvider",fileLocation)
+                    val sendIntent = Intent(Intent.ACTION_SEND)
+                    sendIntent.setType("text/csv")
+                    sendIntent.putExtra(Intent.EXTRA_EMAIL,"prueba@hotmail.com")
+                    sendIntent.putExtra(Intent.EXTRA_SUBJECT, "GeoInteligencia. Encuesta contestada " +
+                            "por ${sharedPref.getString(KEY_NAME,".")}")
+                    sendIntent.putExtra(Intent.EXTRA_TEXT,"Enviado desde Geointeligencia App")
+                    sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    sendIntent.putExtra(Intent.EXTRA_STREAM,path)
+                    startActivity(Intent.createChooser(sendIntent,"Send mail"))
 
+                } catch(e: Exception){
+
+                }
                 Toast.makeText(context,"Conectado a Internet",Toast.LENGTH_SHORT).show()
             }
 
